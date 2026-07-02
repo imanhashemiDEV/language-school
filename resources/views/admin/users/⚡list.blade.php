f101592037c554cfa0364b4229add40ec4397550
iman hashemi <hashemi.iman@gmail.com>
2026-07-02 12:50:04 +0330

    users

--- resources/views/admin/users/⚡list.blade.php
+++ resources/views/admin/users/⚡list.blade.php
@@ -28,12 +28,12 @@ class extends Component {
                             کاربران
                         </div>
                         <div class="flex flex-col gap-x-3 gap-y-2 sm:flex-row rtl:md:mr-auto ltr:md:ml-auto">
-                            <button data-tw-merge=""
+                            <a href="{{route('admin.users.create')}}" data-tw-merge=""
                                     class="transition duration-200 border shadow-sm inline-flex items-center justify-center py-2 px-3 rounded-md font-medium cursor-pointer focus:ring-4 focus:ring-primary focus:ring-opacity-20 focus-visible:outline-none dark:focus:ring-slate-700 dark:focus:ring-opacity-50 [&:hover:not(:disabled)]:bg-opacity-90 [&:hover:not(:disabled)]:border-opacity-90 [&:not(button)]:text-center disabled:opacity-70 disabled:cursor-not-allowed bg-primary border-primary text-white dark:border-primary group-[.mode--light]:!border-transparent group-[.mode--light]:!bg-white/[0.12] group-[.mode--light]:!text-slate-200">
                                 <i data-tw-merge="" data-lucide="pen-line"
                                    class="rtl:ml-2 ltr:mr-2 h-4 w-4 stroke-[1.3]"></i>
-                                افوزدن کاربری جدید
-                            </button>
+                                افزودن کاربری جدید
+                            </a>
                         </div>
                     </div>
                     <div class="mt-3.5 flex flex-col gap-8">
@@ -42,7 +42,7 @@ class extends Component {
                                 <div
                                     class="box col-span-4 rounded-[0.6rem] border border-dashed border-slate-300/80 p-5 shadow-sm md:col-span-2 xl:col-span-1">
                                     <div class="text-base text-slate-500">کاربران ثبت‌نام کرده</div>
-                                    <div class="mt-1.5 text-2xl font-medium">457,204</div>
+                                    <div class="mt-1.5 text-2xl font-medium">{{$this->users->count()}}</div>
                                     <div
                                         class="absolute inset-y-0 rtl:left-0 ltr:right-0 rtl:ml-5 ltr:mr-5 flex flex-col justify-center">
                                         <div
@@ -56,7 +56,7 @@ class extends Component {
                                 <div
                                     class="box col-span-4 rounded-[0.6rem] border border-dashed border-slate-300/80 p-5 shadow-sm md:col-span-2 xl:col-span-1">
                                     <div class="text-base text-slate-500">فعال کاربران</div>
-                                    <div class="mt-1.5 text-2xl font-medium">122,721</div>
+                                    <div class="mt-1.5 text-2xl font-medium">{{$this->users->where('is_active', true)->count()}}</div>
                                     <div
                                         class="absolute inset-y-0 rtl:left-0 ltr:right-0 rtl:ml-5 ltr:mr-5 flex flex-col justify-center">
                                         <div
--- routes/admin.php
+++ routes/admin.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::livewire('/', 'admin::panel.index')->name("admin.panel.index");
 Route::livewire('/users', 'admin::users.list')->name('admin.users.list');
+Route::livewire('/create_users', 'admin::users.create')->name('admin.users.create');
+Route::livewire('/edit_users', 'admin::users.edit')->name('admin.users.edit');
